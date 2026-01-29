3be43f0c9307e2552cfb1d799397b66cb6b9e13d
Hotpot94 <mallahrahul@gmail.com>
2026-01-29 16:53:22 +0800

    Completed section 15, Added security configuration lecture 200

---
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,60 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street must be at least 5 characters.")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must be at least 5 characters.")
+    private String building;
+
+    @NotBlank
+    @Size(min = 4, message = "city name must be at least 4 characters.")
+    private String city;
+
+    @NotBlank
+    @Size(min = 2, message = "State name must be at least 2 characters.")
+    private String state;
+
+    @NotBlank
+    @Size(min = 2, message = "Country name must be at least 2 characters.")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "Zipcode must be at least 6 characters.")
+    private String zipcode;
+
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String building, String city, String state, String country, String zipcode) {
+        this.street = street;
+        this.building = building;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipcode = zipcode;
+    }
+}
